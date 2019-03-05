0adbad569ae8cf42f3c19ce682cfff0e6f3b48d4
Faz <faz888@gmail.com>
2019-03-05 17:09:31 +0000

    using devour client

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,8 +1,40 @@
+// import JsonApi from 'devour-client';
+const JsonApi = require('devour-client')
+
 const dashboardRouter = (req, res) => {
+
+  const jsonApi = new JsonApi({
+    apiUrl: 'https://stakeholder-engagement-api.herokuapp.com/api/v1/'
+  });
+  let requestMiddleware = {
+    name: 'add-headers',
+    req: (payload) => {
+      if (payload.req.method === 'GET') {
+        payload.req.headers = {
+          "Accept" : "application/json"
+        }
+      }
+      return payload
+    }
+  };
+  jsonApi.insertMiddlewareBefore('axios-request', requestMiddleware)
+
+  jsonApi.define('region', {
+    id: '',
+    slug: '',
+    name: '',
+    nuts_code: '',
+    created_at: '',
+    updated_at: ''
+  });
+
+  const regions = jsonApi.findAll('region');
+
   const params = {
-    message: `Hello, world!`
-  }
-  res.render('app/views/dashboard', params)
+    regions
+  };
+
+  res.render('app/views/dashboard', params);
 };
 
 module.exports = dashboardRouter;
