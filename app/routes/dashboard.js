701a1819a25bb1b05d507fce5c8c06333b65033a
Faz <faz888@gmail.com>
2019-03-07 18:41:45 +0000

    use async and promises to get all data before view is rendered

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,9 +1,11 @@
-const JsonApi = require('devour-client');
+const Devour = require('devour-client');
 const npmPkg = require('../../package.json');
 
 const dashboardRouter = (req, res) => {
-  const jsonApi = new JsonApi({
-    apiUrl: 'https://stakeholder-engagement-api.herokuapp.com/api/v1'
+  const params = {};
+  const jsonApi = new Devour({
+    apiUrl: 'https://stakeholder-engagement-api.herokuapp.com/api/v1',
+    'user-agent': `stakeholder-engagement-frontend/${npmPkg.version} (https://github.com/communitiesuk/stakeholder-engagement-frontend)`
   });
 
   // model
@@ -13,15 +15,23 @@ const dashboardRouter = (req, res) => {
     name: "",
     nuts_code: "",
     created_at: "",
-    updated_at: ""
+    updated_at: "",
+    links : {}
   });
 
-  // -- deconstruct response
-  jsonApi.findAll('region')
-  .then(function(response) {
-    let { data, errors, meta, links } = response;
-    res.render('app/views/dashboard', {regions : data});
-  });
+  (async () => {
+    const responses = await Promise.all([
+      jsonApi.findAll('region')
+    ]);
+
+    params.data = responses.map((response) => {
+      return { data, errors, meta, links } = response;
+    })
+
+    res.render('app/views/dashboard', params);
+
+  })();
+
 };
 
 module.exports = dashboardRouter;
